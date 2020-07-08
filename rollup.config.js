671a7892ef9df2870b2a8f38e4e0cd6bfdc9ed57
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-07-07 22:30:37 -0500

    using multi-entry

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import { babel } from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import autoExternal from 'rollup-plugin-auto-external'
+import multi from '@rollup/plugin-multi-entry';
 
 const plugins = [
 	autoExternal({
@@ -12,6 +13,8 @@ const plugins = [
 	}),
 
 	babel({ extensions: ['.js', '.coffee'], babelHelpers: 'bundled' }),
+
+	multi(),
 
 	// so Rollup can find externals
 	resolve({ extensions: ['ts', '.js', '.coffee'], preferBuiltins: true }),
@@ -36,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 
 export default [
 	{
-		input: 'src/x-terminal.js',
+		input: ['src/x-terminal.js', 'src/*.js'],
 		output: [
 			{
 				dir: 'dist',
